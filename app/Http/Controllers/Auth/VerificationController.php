09f920240502c6a9ab81592247c6da94cdb64de1
Nitesh K. Yagol <nitzsth2@gmail.com>
2019-01-30 11:03:38 +0545

    Prepare project structure as our need

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Helpers\Constant;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\VerifiesEmails;
 
@@ -25,7 +26,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = Constant::REDIRECT_AUTHENTICATED;
 
     /**
      * Create a new controller instance.
